67583b9164863e57e2e4b8014588ccf8b7e47460
Mayerlyn R <maye.yrs23@gmail.com>
2025-04-23 10:04:04 -0400

    variable de entorno SECRET_KEY

--- siete/settings_test.py
+++ siete/settings_test.py
@@ -1,5 +1,8 @@
 from siete.settings import *
 
+# Establecer SECRET_KEY para pruebas
+SECRET_KEY = "django-insecure-kxg!z+=471m9_nhc#$01qrxli8xb@hxqe--oalys$fv5fqf!g("
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
